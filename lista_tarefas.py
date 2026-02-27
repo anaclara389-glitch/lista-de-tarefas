e2eb954c9067fbcdc0b45d12479286eff1a1c770
anaclara389-glitch <abreuanaclara389@gmail.com>
2026-02-26 21:50:48 -0300

    novo commit

--- lista_tarefas.py
+++ lista_tarefas.py
@@ -4,11 +4,12 @@ tarefas = []
 tarefas.append("Estudar Python")
 tarefas.append("Ir ao ginásio")
 tarefas.append("Comprar pão")
+tarefas.append("Fazer o dever de casa")
 
 print("Minha lista atual:", tarefas)
 
 # Removemos o primeiro item (índice 0)
-tarefas.pop(0)
+tarefas.pop(1)
 
 print("\nLista atualizada:")
 for item in tarefas:
